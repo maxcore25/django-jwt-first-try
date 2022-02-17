6aa541beae084ebf292d40c2320e40c2dc4c6392
maxcore25 <mkorobov2002@mail.ru>
2022-02-17 06:58:16 +0300

    JWT Authentication written

--- users/views.py
+++ users/views.py
@@ -45,4 +45,17 @@ class LoginView(APIView):
 
 class UserView(APIView):
     def get(self, request):
-        pass
+        print(request.COOKIES)
+        token = request.COOKIES.get('jwt')
+
+        if not token:
+            raise AuthenticationFailed('Unauthenticated')
+
+        try:
+            payload = jwt.decode(token, 'secret', algorithms=['HS256'])
+        except jwt.ExpiredSignatureError:
+            raise AuthenticationFailed('Unauthenticated')
+
+        user = User.objects.filter(id=payload['id']).first()
+        serializer = UserSerializer(user)
+        return Response(serializer.data)
